d039c10325b1ebf558b01ef39ee6d5a8c4b9e5a4
Andre Markov <andremarkov@icloud.com>
2025-01-23 11:11:28 +0300

    upd icon

--- app/src/androidTest/java/com/wh/pradius/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/wh/pradius/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.terminal.pradius", appContext.getPackageName());
+        assertEquals("com.wh.pradius", appContext.getPackageName());
     }
 }
